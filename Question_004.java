2e65851f2bd3882baeecba3a9309b9d03f445144
Juan Romo <juanromo.dev@gmail.com>
2018-07-01 22:26:18 -0500

    Question 4 completed

---
+++ Question_004.java
@@ -0,0 +1,28 @@
+public class Question_004 {
+	public static void main(String[] args) {
+		solutionOne();
+	}
+
+	static void solutionOne() {
+		int largestPalindromeProduct = -1;
+
+		for (int i = 1; i < 1_000; i++) {
+			for (int j = 1; j < 1_000; j++) {
+				int product = i * j;
+
+				if (isPalindrome(product)) {
+					if (product > largestPalindromeProduct) {
+						largestPalindromeProduct = product;
+					}
+				}
+			}
+		}
+
+		System.out.println(largestPalindromeProduct);
+	}
+
+	static boolean isPalindrome(int number) {
+		String numberStr = String.valueOf(number);
+		return numberStr.equals(new StringBuilder(numberStr).reverse().toString());
+	}
+}
