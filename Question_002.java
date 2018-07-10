fc50b4464ef56538bde520bad08167fad7622dcf
Juan Romo <juanromo.dev@gmail.com>
2018-07-10 15:29:06 -0500

    Additional solution added to question 2

--- Question_002.java
+++ Question_002.java
@@ -7,6 +7,7 @@ public class Question_002 {
 
 	public static void main(String[] args) {
 		solutionOne();
+		solutionTwo();
 	}
 
 	static void solutionOne() {
@@ -45,5 +46,34 @@ public class Question_002 {
 		}
 
 		System.out.println(sum);
+	}
+
+	static void solutionTwo() {
+		long sum = 0;
+
+		// Calculate Fibonacci numbers recursively.
+		for (int i = 1; ; i++) {
+			int result = fibonacci(i);
+
+			if (result > LIMIT) {
+				break;
+			}
+
+			if (result % 2 == 0) {
+				sum += result;
+			}
+		}
+
+		System.out.println(sum);
+	}
+
+	static int fibonacci(int n) {
+		if (n == 1) {
+			return F1;
+		} else if (n == 2) {
+			return F2;
+		} else {
+			return fibonacci(n - 1) + fibonacci(n - 2);
+		}
 	}
 }
