38ab2982ab32843e8b52859b43e3b5d7eb4cfc2f
Juan Romo <juanromo.dev@gmail.com>
2018-07-10 13:15:25 -0500

    Refactored question 1

--- Question_001.java
+++ Question_001.java
@@ -1,4 +1,7 @@
 public class Question_001 {
+
+	static final int LIMIT = 1_000;
+
 	public static void main(String[] args) {
 		solutionOne();
 		solutionTwo();
@@ -8,7 +11,7 @@ public class Question_001 {
 		int sum = 0;
 
 		// Add numbers divisible by 3 or 5.
-		for (int i = 0; i < 1000; i++) {
+		for (int i = 0; i < LIMIT; i++) {
 			if (i % 3 == 0 || i % 5 == 0) {
 				sum += i;
 			}
@@ -21,21 +24,21 @@ public class Question_001 {
 		int sum = 0;
 
 		// Add numbers divisible by 3.
-		for (int i = 0; i < 1000; i += 3) {
+		for (int i = 0; i < LIMIT; i += 3) {
 			if (i % 3 == 0) {
 				sum += i;
 			}
 		}
 
 		// Add numbers divisible of 5.
-		for (int i = 0; i < 1000; i += 5) {
+		for (int i = 0; i < LIMIT; i += 5) {
 			if (i % 5 == 0) {
 				sum += i;
 			}
 		}
 
 		// Remove overlapping divisors shared by 3 and 5.
-		for (int i = 0; i < 1000; i += 15) {
+		for (int i = 0; i < LIMIT; i += 15) {
 			if (i % 15 == 0) {
 				sum -= i;
 			}
