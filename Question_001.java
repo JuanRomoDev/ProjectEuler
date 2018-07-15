37161687a2e3f24a3399c91a86acb85c07f85f44
Juan Romo <juanromo.dev@gmail.com>
2018-07-14 21:53:50 -0500

    Refactored question 1

--- Question_001.java
+++ Question_001.java
@@ -1,49 +1,48 @@
 public class Question_001 {
 
-	static final int LIMIT = 1_000;
+  private static final int LIMIT = 1_000;
 
-	public static void main(String[] args) {
-		solutionOne();
-		solutionTwo();
-	}
+  public static void main(String[] args) {
+    solutionTwo();
+  }
 
-	static void solutionOne() {
-		int sum = 0;
+  private static void solutionOne() {
+    int sum = 0;
 
-		// Add numbers divisible by 3 or 5.
-		for (int i = 0; i < LIMIT; i++) {
-			if (i % 3 == 0 || i % 5 == 0) {
-				sum += i;
-			}
-		}
+    // Add numbers divisible by 3 or 5.
+    for (int i = 0; i < LIMIT; i++) {
+      if (i % 3 == 0 || i % 5 == 0) {
+        sum += i;
+      }
+    }
 
-		System.out.println(sum);
-	}
+    System.out.println(sum);
+  }
 
-	static void solutionTwo() {
-		int sum = 0;
+  private static void solutionTwo() {
+    int sum = 0;
 
-		// Add numbers divisible by 3.
-		for (int i = 0; i < LIMIT; i += 3) {
-			if (i % 3 == 0) {
-				sum += i;
-			}
-		}
+    // Add numbers divisible by 3.
+    for (int i = 0; i < LIMIT; i += 3) {
+      if (i % 3 == 0) {
+        sum += i;
+      }
+    }
 
-		// Add numbers divisible of 5.
-		for (int i = 0; i < LIMIT; i += 5) {
-			if (i % 5 == 0) {
-				sum += i;
-			}
-		}
+    // Add numbers divisible of 5.
+    for (int i = 0; i < LIMIT; i += 5) {
+      if (i % 5 == 0) {
+        sum += i;
+      }
+    }
 
-		// Remove overlapping divisors shared by 3 and 5.
-		for (int i = 0; i < LIMIT; i += 15) {
-			if (i % 15 == 0) {
-				sum -= i;
-			}
-		}
+    // Remove overlapping divisors shared by 3 and 5.
+    for (int i = 0; i < LIMIT; i += 15) {
+      if (i % 15 == 0) {
+        sum -= i;
+      }
+    }
 
-		System.out.println(sum);
-	}
+    System.out.println(sum);
+  }
 }
